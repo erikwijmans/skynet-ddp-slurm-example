4cba4590d87e9bf844bf5c9a37c454a4101925fa
Erik Wijmans <etw@gatech.edu>
2021-07-25 10:10:09 -0400

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=cifar10-ddp
 #SBATCH --output=logs.out
 #SBATCH --error=logs.err
-#SBATCH --gres gpu:2
+#SBATCH --gpus-per-task 1  # This gives 1 GPU to each process (or task)
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node 2
+#SBATCH --ntasks-per-node 2 # The number of processes for slurm to start on each node
 #SBATCH --partition=short
 
 
 export MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
 
 set -x
-srun python -u -m ddp_example.train_cifar10
+srun -u python -u -m ddp_example.train_cifar10
